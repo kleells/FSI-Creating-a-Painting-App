ff7dd50461f8bea4f8af562eca6553a387873ea9
Katie Eells <katieeells@Katies-MBP.lan>
2022-01-31 21:22:30 -0500

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let selectedColor = "blue";
 let painting = document.querySelector(".painting");
 painting.addEventListener("click", function (e) {
-  e.target.style.backgroundColor = selectedColor;
+	e.target.style.backgroundColor = selectedColor;
 });
+
+let colorChoices =
+	document.querySelectorAll(".color-choice");
+for (let i = 0; i < colorChoices.length; i++) {
+	colorChoices[i].addEventListener("click", function () {
+		selectedColor = colorChoices[i].id;
+	});
+}
+
+//Katie's code below
 
 // let colorChoices = document.querySelectorAll(".color-choice");
 // for (let i = 0; i < colorChoices.length; i++) {
@@ -11,7 +21,7 @@ painting.addEventListener("click", function (e) {
 //   });
 // }
 
-let palette = document.querySelector(".palette");
-palette.addEventListener("click", function (e) {
-  selectedColor = e.target.id;
-});
+//let palette = document.querySelector(".palette");
+//palette.addEventListener("click", function (e) {
+//selectedColor = e.target.id;
+//});
